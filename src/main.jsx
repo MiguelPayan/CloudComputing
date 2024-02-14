2ac15b7433e43859502cfb55ce6498ca8907ff07
unknown <L20550629@chihuahua2.tecnm.mx>
2024-02-14 14:31:33 -0600

    Cambios

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+<h1>Miguel</h1>
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
